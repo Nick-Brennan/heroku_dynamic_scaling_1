40caf3a63db3b5d529ce0e6c5c48aecdd4f956d8
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 13:17:46 -0800

    trying to change dynos in right order

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,7 +4,7 @@ class TestController < ApplicationController
   	p "ok 1"
   	var1 = system 'heroku ps --app cli-test-1'
   	p "ok 2"
-  	var2 = system 'heroku ps:scale web=1:standard-1x --app cli-test-1'
+  	var2 = system 'heroku ps:resize web=standard-1x --app cli-test-1'
   	var2 = system 'heroku ps:scale web1=1:standard-1x --app cli-test-1'
   	var3 = system 'heroku ps:scale web2=1:standard-1x --app cli-test-1'
   	# var3 = system 'ps'
@@ -23,7 +23,7 @@ class TestController < ApplicationController
   	var2 = system 'heroku ps:scale web1=0 --app cli-test-1'
   	var3 = system 'heroku ps:stop web2 --app cli-test-1'
   	var4 = system 'heroku ps:scale web2=0 --app cli-test-1'
-  	var5 = system 'heroku ps:resize web=hobby'
+  	var5 = system 'heroku ps:resize web=hobby --app cli-test-1'
   	p var1
   	p var2
   	p var3
