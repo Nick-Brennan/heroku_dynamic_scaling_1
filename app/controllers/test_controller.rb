fba1617807f702f08506b55dcb2039e3b43fabbc
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 11:42:32 -0800

    logging

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,6 +1,9 @@
 class TestController < ApplicationController
   def test
-  	system 'heroku ps:scale web1=1'
+  	var1 = system 'heroku ps'
+  	var2 = system 'heroku ps:scale web1=1'
+  	p var1
+  	p var2
   	render :inline => "<h1> Test </h1>"
   end
 end
