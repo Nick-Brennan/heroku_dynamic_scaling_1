5f83fd206b868b43b0934f566d9bd956cb9fce61
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 13:14:02 -0800

    mess with dyno size

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,7 +4,8 @@ class TestController < ApplicationController
   	p "ok 1"
   	var1 = system 'heroku ps --app cli-test-1'
   	p "ok 2"
-  	var2 = system 'heroku ps:scale web1=1 --app cli-test-1'
+  	var2 = system 'heroku ps:scale web=1:standard-1x --app cli-test-1'
+  	var2 = system 'heroku ps:scale web1=1:standard-1x --app cli-test-1'
   	var3 = system 'heroku ps:scale web2=1:standard-1x --app cli-test-1'
   	# var3 = system 'ps'
   	# var4 = system 'scale web1=1:standard-1x'
@@ -22,10 +23,12 @@ class TestController < ApplicationController
   	var2 = system 'heroku ps:scale web1=0 --app cli-test-1'
   	var3 = system 'heroku ps:stop web2 --app cli-test-1'
   	var4 = system 'heroku ps:scale web2=0 --app cli-test-1'
+  	var5 = system 'heroku ps:resize web=hobby'
   	p var1
   	p var2
   	p var3
   	p var4
+  	p var5
   	render :inline => "<h1> Check Web UI </h1>"
   end
 end
