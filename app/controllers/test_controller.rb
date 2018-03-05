20c67960d30e860e66e0e50a929e3712666b9eb5
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 11:47:20 -0800

    fiddling

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,8 +2,12 @@ class TestController < ApplicationController
   def test
   	var1 = system 'heroku ps'
   	var2 = system 'heroku ps:scale web1=1'
+  	var3 = system 'ps'
+  	var4 = system 'ps:scale web1=1'
   	p var1
   	p var2
+  	p var3
+  	p var4
   	render :inline => "<h1> Test </h1>"
   end
 end
