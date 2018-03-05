2d9a2d311005dc915bcecbef99c5462d91632c7f
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 12:04:46 -0800

    tweak

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,15 +1,17 @@
 class TestController < ApplicationController
   def test
   	# added a buildpack
+  	p "ok 1"
   	var1 = system 'heroku ps'
+  	p "ok 2"
   	var2 = system 'heroku ps:scale web1=1'
-  	var3 = system 'ps'
-  	var4 = system 'scale web1=1:standard-1x'
-  	system 'ps --help simple'
+  	# var3 = system 'ps'
+  	# var4 = system 'scale web1=1:standard-1x'
+  	# system 'ps --help simple'
   	p var1
   	p var2
-  	p var3
-  	p var4
+  	# p var3
+  	# p var4
   	render :inline => "<h1> Test </h1>"
   end
 end
