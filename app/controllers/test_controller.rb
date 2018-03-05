72af5f691d02f086c82c4e1aa91031599182a488
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 12:10:42 -0800

    specify app in cli command

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,9 +2,9 @@ class TestController < ApplicationController
   def test
   	# added a buildpack
   	p "ok 1"
-  	var1 = system 'heroku ps'
+  	var1 = system 'heroku ps --app cli-test-1'
   	p "ok 2"
-  	var2 = system 'heroku ps:scale web1=1'
+  	var2 = system 'heroku ps:scale web1=1 --app cli-test-1'
   	# var3 = system 'ps'
   	# var4 = system 'scale web1=1:standard-1x'
   	# system 'ps --help simple'
