6fd59bf4b262ab387f57cbe35b1b62fdf8d57130
Nick-Brennan <nick_p_b@yahoo.com>
2018-03-05 13:02:36 -0800

    try with 2 dynos

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -5,22 +5,27 @@ class TestController < ApplicationController
   	var1 = system 'heroku ps --app cli-test-1'
   	p "ok 2"
   	var2 = system 'heroku ps:scale web1=1 --app cli-test-1'
+  	var3 = system 'heroku ps:scale web2=1 --app cli-test-1'
   	# var3 = system 'ps'
   	# var4 = system 'scale web1=1:standard-1x'
   	# system 'ps --help simple'
   	p var1
   	p var2
-  	# p var3
+  	p var3
   	# p var4
   	render :inline => "<h1> Test </h1>"
   end
 
   def off
-  	p "swithing dyno off"
+  	p "switching dyno off"
   	var1 = system 'heroku ps:stop web1 --app cli-test-1'
   	var2 = system 'heroku ps:scale web1=0 --app cli-test-1'
+  	var3 = system 'heroku ps:stop web2 --app cli-test-1'
+  	var4 = system 'heroku ps:scale web2=0 --app cli-test-1'
   	p var1
   	p var2
+  	p var3
+  	p var4
   	render :inline => "<h1> Check Web UI </h1>"
   end
 end
